ed21270c1561348760980f7e7076a1f1d78d040f
bezumnui <Bezumnui.mistikgt@gmail.com>
2022-09-21 00:10:00 +0400

    mega compress files

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-source venv/bin/activate
-gunicorn 'app:app' -w 10 -b localhost:9990
+#git pull
+
+
+
+while getopts u: arg
+do
+  case arg in
+  u) git pull;;
+  s) source venv bin activate;;
+  esac
+done
+
+./start.sh
+
