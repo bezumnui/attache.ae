7e7634c98c759b827759bdc6b54c55c307a9634a
bezumnui <Bezumnui.mistikgt@gmail.com>
2022-09-20 19:38:37 +0400

    new animations.js

--- static/scripts/animations.js
+++ static/scripts/animations.js
@@ -11,26 +11,42 @@ async function undo(sl, prevId, afterId, doAfter) {
     }
 }
 
-async function onCross() {
-    document.getElementById("cross").style.display = "none";
-    document.getElementById("navID").style.animation = "remove-menu .2s linear 1";
-    await sleep(200);
-    document.getElementById("navID").style.display = "none";
-    document.getElementById("navID").style.animation = "";
-    
+async function onCross(immediately) {
+    let navID = document.querySelector("#navID");
+    let cross = document.querySelector("#cross");
+    cross.style.display = "none";
+    if (immediately === true) {
+        navID.style.display = "none";
+        return;
+    }
+    navID.classList.toggle("slide-out");
+    const animationEndCallback = (e) => {
+        navID.removeEventListener('animationend', animationEndCallback);
+
+        navID.classList.remove('slide-out');
+        navID.style.display = "none";
+    }
+    navID.addEventListener("animationend", animationEndCallback)
 }
 
 async function onBurger() {
-    console.log(123);
     // document.getElementById("burger").style.animationName = "burger-anim";
     // document.getElementById("burger").style.animationDelay = .3;
-    document.getElementById("burger").style.animation = "burger-anim .2s linear 1";
-    document.getElementById("navID").style.display = "flex"
-    document.getElementById("navID").style.animation = "display-menu .2s linear 1";
-    document.getElementById("cross").style.display = "block";
-    await sleep(200);
-    document.getElementById("burger").style.animation = "";
-    document.getElementById("navID").style.display = "flex";
-    document.getElementById("navID").style.animation = "";
+    // document.getElementById("burger").style.animation = "burger-anim .2s linear 1";
+
+    let navID = document.querySelector("#navID");
+    let cross = document.querySelector("#cross");
+    navID.style.display = "flex";
+    cross.style.display = "block";
+
+    navID.classList.toggle("slide-in")
+
+    let animationEndCallback = (e) => {
+        navID.removeEventListener('animationend', animationEndCallback);
+        navID.classList.remove('slide-in');
+
+
+    }
+    navID.addEventListener('animationend', animationEndCallback)
 
 }
