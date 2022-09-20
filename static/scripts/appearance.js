33dfff9125e543771a8e96b8ecb525e70dfd51bd
bezumnui <Bezumnui.mistikgt@gmail.com>
2022-09-20 17:25:33 +0400

    change uri to static/

--- static/scripts/appearance.js
+++ static/scripts/appearance.js
@@ -87,26 +87,26 @@ function onAppear(first) {
         r.style.setProperty("--second-color", "#0082ca");
         r.style.setProperty("--main-text-color", "white");
         r.style.setProperty("--second-text-color", "black");
-        r.style.setProperty("--appear-image", "url(\"https://static.attache.ae/icons/sun.svg\")");
-        r.style.setProperty("--burger-icon", "url(\"https://static.attache.ae/icons/burger.svg\")");
-        r.style.setProperty("--head-logo-image", "url(\"https://static.attache.ae/images/head_logo.png\")");
-        r.style.setProperty("--speed-icon", "url(\"https://static.attache.ae/icons/speed.svg\")");
+        r.style.setProperty("--appear-image", "url(\"static/icons/sun.svg\")");
+        r.style.setProperty("--burger-icon", "url(\"static/icons/burger.svg\")");
+        r.style.setProperty("--head-logo-image", "url(\"static/images/head_logo.png\")");
+        r.style.setProperty("--speed-icon", "url(\"static/icons/speed.svg\")");
 
-        r.style.setProperty("--prepair-image", "url(https://static.attache.ae/icons/white-papers.svg)");
-        r.style.setProperty("--agency-image", "url(https://static.attache.ae/icons/peoples.svg)");
-        r.style.setProperty("--services-image", "url(https://static.attache.ae/icons/tags.svg)");
-        r.style.setProperty("--contacts-image", "url(https://static.attache.ae/icons/black-phone.svg)");
-        r.style.setProperty("--contacts-image-mobi", "url(https://static.attache.ae/icons/phone.svg)");
+        r.style.setProperty("--prepair-image", "url(static/icons/white-papers.svg)");
+        r.style.setProperty("--agency-image", "url(static/icons/peoples.svg)");
+        r.style.setProperty("--services-image", "url(static/icons/tags.svg)");
+        r.style.setProperty("--contacts-image", "url(static/icons/black-phone.svg)");
+        r.style.setProperty("--contacts-image-mobi", "url(static/icons/phone.svg)");
 
-        r.style.setProperty("--hotel-image", "url(https://static.attache.ae/images/hotel.webp)");
+        r.style.setProperty("--hotel-image", "url(static/images/hotel.webp)");
 
 
-        r.style.setProperty("--item1-icon", "url(https://static.attache.ae/icons/company_one.svg)");
-        r.style.setProperty("--item2-icon", "url(https://static.attache.ae/icons/company_two.svg)");
-        r.style.setProperty("--item3-icon", "url(https://static.attache.ae/icons/visa_four.svg)");
-        r.style.setProperty("--item4-icon", "url(https://static.attache.ae/icons/visa_six.svg)");
-        r.style.setProperty("--item5-icon", "url(https://static.attache.ae/icons/visa.svg)");
-        r.style.setProperty("--item6-icon", "url(https://static.attache.ae/icons/cert.svg)");
+        r.style.setProperty("--item1-icon", "url(static/icons/company_one.svg)");
+        r.style.setProperty("--item2-icon", "url(static/icons/company_two.svg)");
+        r.style.setProperty("--item3-icon", "url(static/icons/visa_four.svg)");
+        r.style.setProperty("--item4-icon", "url(static/icons/visa_six.svg)");
+        r.style.setProperty("--item5-icon", "url(static/icons/visa.svg)");
+        r.style.setProperty("--item6-icon", "url(static/icons/cert.svg)");
   
 
         nowAppear = "black";
@@ -119,25 +119,25 @@ function onAppear(first) {
         r.style.setProperty("--second-color", "#dd8679");
         r.style.setProperty("--main-text-color", "black");
         r.style.setProperty("--second-text-color", "white");
-        r.style.setProperty("--appear-image", "url(\"https://static.attache.ae/icons/moon.svg\")");
-        r.style.setProperty("--burger-icon", "url(\"https://static.attache.ae/icons/bburger.svg\")");
-        r.style.setProperty("--head-logo-image", "url(\"https://static.attache.ae/images/bhead_logo.png\")");
-        r.style.setProperty("--speed-icon", "url(\"https://static.attache.ae/icons/bspeed.svg\")");
+        r.style.setProperty("--appear-image", "url(\"static/icons/moon.svg\")");
+        r.style.setProperty("--burger-icon", "url(\"static/icons/bburger.svg\")");
+        r.style.setProperty("--head-logo-image", "url(\"static/images/bhead_logo.png\")");
+        r.style.setProperty("--speed-icon", "url(\"static/icons/bspeed.svg\")");
 
-        r.style.setProperty("--prepair-image", "url(https://static.attache.ae/icons/black-papers.svg)");
-        r.style.setProperty("--agency-image", "url(https://static.attache.ae/icons/black-peoples.svg)");
-        r.style.setProperty("--services-image", "url(https://static.attache.ae/icons/black-tags.svg)");
-        r.style.setProperty("--contacts-image", "url(https://static.attache.ae/icons/phone.svg)");
-        r.style.setProperty("--contacts-image-mobi", "url(https://static.attache.ae/icons/black-phone.svg)");
-        r.style.setProperty("--hotel-image", "url(https://static.attache.ae/images/white-hotel.webp)");
+        r.style.setProperty("--prepair-image", "url(static/icons/black-papers.svg)");
+        r.style.setProperty("--agency-image", "url(static/icons/black-peoples.svg)");
+        r.style.setProperty("--services-image", "url(static/icons/black-tags.svg)");
+        r.style.setProperty("--contacts-image", "url(static/icons/phone.svg)");
+        r.style.setProperty("--contacts-image-mobi", "url(static/icons/black-phone.svg)");
+        r.style.setProperty("--hotel-image", "url(static/images/white-hotel.webp)");
 
 
-        r.style.setProperty("--item1-icon", "url(https://static.attache.ae/icons/bcompany_one.svg)");
-        r.style.setProperty("--item2-icon", "url(https://static.attache.ae/icons/bcompany_two.svg)");
-        r.style.setProperty("--item3-icon", "url(https://static.attache.ae/icons/bvisa_four.svg)");
-        r.style.setProperty("--item4-icon", "url(https://static.attache.ae/icons/bvisa_six.svg)");
-        r.style.setProperty("--item5-icon", "url(https://static.attache.ae/icons/bvisa.svg)");
-        r.style.setProperty("--item6-icon", "url(https://static.attache.ae/icons/bcert.svg)");
+        r.style.setProperty("--item1-icon", "url(static/icons/bcompany_one.svg)");
+        r.style.setProperty("--item2-icon", "url(static/icons/bcompany_two.svg)");
+        r.style.setProperty("--item3-icon", "url(static/icons/bvisa_four.svg)");
+        r.style.setProperty("--item4-icon", "url(static/icons/bvisa_six.svg)");
+        r.style.setProperty("--item5-icon", "url(static/icons/bvisa.svg)");
+        r.style.setProperty("--item6-icon", "url(static/icons/bcert.svg)");
 
         nowAppear = "white";
         setCookie("appear", "white", 10);
